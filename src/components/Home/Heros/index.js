9a3ed61b870208b319b64c1791acc370eb8114f4
José Nunes <josenunes34@gmail.com>
2017-04-08 18:30:16 +0100

    First commit

--- src/components/Home/Heros/index.js
+++ src/components/Home/Heros/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Carousel from 're-carousel';
-import IndicatorDots from './IndicatorDots';
+import Slider from 'react-slick';
 import Hero from './Hero';
 import LangMenu from '/components/common/LangMenu';
 import styles from './Heros.css';
@@ -8,15 +7,25 @@ import { heros } from '/jn.config';
 
 const Heros = () => {
   const frames = heros.map(hero =>
-    <Hero
-      key={hero.stringKey}
-      hero={hero}
-    />,
+    <div key={hero.stringKey}>
+      <Hero hero={hero} />
+    </div>,
   );
+  const settings = {
+    dots: true,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div className={styles.root}>
       <div className={styles.carouselContainer}>
-        <Carousel auto frames={frames} indicator={IndicatorDots} interval={5000} />
+        <Slider {...settings}>
+          {frames}
+        </Slider>
       </div>
       <div className={styles.langMenu}>
         <div className="container" style={{ textAlign: 'right' }}>
