3798d572d99640be59633e08d1e1447d949b68a9
Jose Nunes <josenunes34@gmail.com>
2018-09-15 19:29:56 +0100

    #9 Add FooterMe and patterns

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,18 @@
 import React from 'react';
+import FooterMe from '../components/common/FooterMe';
+
+import styles from './index.module.scss';
 
 export default () => (
-  <div>
-    <h1>Hello world</h1>
-    <p>
-      Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
-      justo sit amet risus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
-      lacinia odio sem nec elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
-      vestibulum. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh
-      ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna.
-    </p>
-    <h2>Lorem ipsum dolor sit amet</h2>
-    <p>
-      Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem
-      lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus.
-    </p>
-    <h3>Lorem ipsum dolor sit amet</h3>
-    <p>
-      Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem
-      lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus.
-    </p>
-    <h4>Lorem ipsum dolor sit amet</h4>
-    <p>
-      Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem
-      lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus.
-    </p>
+  <div className={styles.root}>
+    <div className={styles.header}>
+      <div className={styles.container}>Header</div>
+    </div>
+    <div className={styles.body} />
+    <div className={styles.footer}>
+      <div className={styles.container}>
+        <FooterMe />
+      </div>
+    </div>
   </div>
 );
