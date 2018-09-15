3c6c0735d6bb5eee6df0ec5734acee3853ca2921
Jose Nunes <josenunes34@gmail.com>
2018-09-15 11:59:01 +0100

    #9 Add Typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-postcss-sass`,
       options: {
         postCssPlugins: [
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
 import React from 'react';
 
-export default () => <div>Hello world!</div>;
+export default () => (
+  <div>
+    <h1>Hello world</h1>
+    <p>
+      Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
+      justo sit amet risus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+      lacinia odio sem nec elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
+      vestibulum. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh
+      ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna.
+    </p>
+    <h2>Lorem ipsum dolor sit amet</h2>
+    <p>
+      Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem
+      lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus.
+    </p>
+    <h3>Lorem ipsum dolor sit amet</h3>
+    <p>
+      Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem
+      lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus.
+    </p>
+    <h4>Lorem ipsum dolor sit amet</h4>
+    <p>
+      Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem
+      lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus.
+    </p>
+  </div>
+);
