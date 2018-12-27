c64e1623a15ff8d988a982c915e0b522c8d50296
Jose Nunes <josenunes34@gmail.com>
2018-12-27 11:02:03 +0000

    #18 Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import Layout from '../components/layouts/layout';
+import LinkButton from '../components/LinkButton';
+
+const StyledNotFound = styled.div`
+  h1 {
+    font-size: 72px;
+    color: ${props => props.theme.neutral100};
+  }
+
+  h2 {
+    font-size: 24px;
+    color: ${props => props.theme.primary500};
+    font-weight: 400;
+  }
+`;
+
+class NotFound extends Component {
+  render() {
+    return (
+      <Layout>
+        <StyledNotFound>
+          <h1>404</h1>
+          <h2>Page not found</h2>
+          <LinkButton to="/">Back to home</LinkButton>
+        </StyledNotFound>
+      </Layout>
+    );
+  }
+}
+
+export default NotFound;
