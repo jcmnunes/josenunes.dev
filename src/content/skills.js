a97f410bb5cd8acb88bd45bd2b32e0181cca7688
Jose Nunes <josenunes34@gmail.com>
2018-12-27 23:37:51 +0000

    Add about page and implement About link.

---
+++ src/content/skills.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    key: '1',
+    name: 'React/Redux',
+    value: '95%',
+  },
+  {
+    key: '2',
+    name: 'React/Redux',
+    value: '95%',
+  },
+  {
+    key: '3',
+    name: 'React/Redux',
+    value: '95%',
+  },
+  {
+    key: '4',
+    name: 'React/Redux',
+    value: '95%',
+  },
+  {
+    key: '5',
+    name: 'React/Redux',
+    value: '95%',
+  },
+  {
+    key: '6',
+    name: 'React/Redux',
+    value: '95%',
+  },
+  {
+    key: '7',
+    name: 'React/Redux',
+    value: '95%',
+  },
+  {
+    key: '8',
+    name: 'React/Redux',
+    value: '95%',
+  },
+  {
+    key: '9',
+    name: 'React/Redux',
+    value: '95%',
+  },
+  {
+    key: '10',
+    name: 'React/Redux',
+    value: '5%',
+  },
+];
