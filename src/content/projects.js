c4bb62e2d46804558db366b56604589f9e58791f
Jose Nunes <josenunes34@gmail.com>
2019-01-03 17:48:15 +0000

    #49 Add links to recent projects on Github

--- src/content/projects.js
+++ src/content/projects.js
@@ -2,13 +2,13 @@ export default [
   {
     key: '1',
     linkText: 'mceuteixeira.com',
-    linkUrl: '/projects/mct',
+    linkUrl: 'https://github.com/jcmnunes/mct',
     auxInfo: 'Personal Website/Blog',
   },
   {
     key: '2',
     linkText: 'josenunes.xyz',
-    linkUrl: '/projects/jnxyz',
+    linkUrl: 'https://github.com/jcmnunes/josenunesxyz',
     auxInfo: 'Personal Website/Blog',
   },
 ];
