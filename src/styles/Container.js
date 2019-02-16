728b7d67426eae144307d45a0146a81ad901a0cc
Jose Nunes <josenunes34@gmail.com>
2019-02-16 19:00:57 +0000

    XYZ-22 Reduce margins for mobile XYZ-24 #close Fixed in XYZ-22

--- src/styles/Container.js
+++ src/styles/Container.js
@@ -6,6 +6,10 @@ const Container = styled.div`
   background-color: ${props => props.theme.gray2};
   margin: 0 auto;
   padding: 96px 32px;
+
+  @media (max-width: ${props => props.theme.bp_small}) {
+    padding: 64px 32px;
+  }
 `;
 
 export default Container;
--- src/styles/Main.js
+++ src/styles/Main.js
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 const Main = styled.div`
   margin-top: 64px;
+
+  @media (max-width: ${props => props.theme.bp_small}) {
+    margin-top: 48px;
+  }
 `;
 
 export default Main;
