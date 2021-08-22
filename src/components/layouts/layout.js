79ee614fc0b3e1f82f9ea3dee9a696c320ba017c
Jose Nunes <josenunes34@gmail.com>
2021-08-22 13:38:00 +0100

    feat: remove blog and full list of articles and courses

--- src/components/layouts/layout.js
+++ src/components/layouts/layout.js
@@ -7,7 +7,7 @@ import { ThemeProvider } from 'styled-components';
 import Logo from '../Logo';
 import Breadcrumb from '../Breadcrumb';
 import PageTransition from '../PageTransition';
-import BlogLink from '../BlogLink';
+// import BlogLink from '../BlogLink';
 
 import theme from '../../styles/theme';
 import Container from '../../styles/Container';
@@ -42,7 +42,7 @@ const Layout = ({ element, props }) => (
             <Header>
               <Logo icon />
               <Breadcrumb />
-              <BlogLink />
+              {/*<BlogLink />*/}
             </Header>
             <PageTransition {...props}>
               <Main>{element}</Main>
